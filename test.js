bc3829e3d50d18f0d52eb5d5cdb09c18101cbc84
0xWarlock <0xWarlock@gmail.com>
2022-04-30 18:14:41 +0000

    trivial turns test

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 const { TestScheduler } = require('rxjs/testing');
-const { throttleTime } = require('rxjs');
+const { interval, map, Subject, takeUntil, throttleTime, timer } = require('rxjs');
 
 const testScheduler = new TestScheduler((actual, expected) => {
   // asserting the two objects are equal - required
@@ -21,3 +21,47 @@ test('generates the stream correctly', () => {
     expectSubscriptions(e1.subscriptions).toBe(e1subs);
   });
 });
+
+function turns(commands) {
+  //const stop = new Subject();
+  const stop = timer(99);
+  return commands.pipe(takeUntil(stop), map(i => `turn ${i + 1}`));
+}
+
+test('takes all commands', (done) => {
+
+  const commands = interval(20);
+  const ts = turns(commands);
+  const recordedTurns = [];
+
+  ts.subscribe(turn => {
+    if (recordedTurns.length == 4) {
+      done('Too many turns');
+    }
+
+    recordedTurns.push(turn);
+    
+    if (recordedTurns.length == 4) {
+      expect(recordedTurns).toEqual(['turn 1', 'turn 2', 'turn 3', 'turn 4']);
+      done();
+    }
+  });
+});
+
+// test('takes all commands', () => {
+//   testScheduler.run((helpers) => {
+//     const { cold, expectObservable } = helpers;
+//     const commands = cold('10ms a 9ms b 9ms c 9ms d|');
+//     const expected =      '10ms a 9ms b 9ms c 9ms d|';
+//     const values = {
+//       a: 1,
+//       b: 2,
+//       c: 3,
+//       d: 4, 
+//     }
+
+//     const turnsObservable = turns(commands).pipe(take(4));
+
+//     expectObservable(turnsObservable).toBe(expected, values);
+//   });
+// });
